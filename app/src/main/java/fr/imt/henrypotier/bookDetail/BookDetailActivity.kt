40326dc88294742934125799134f1659017f19c5
Lucas ROURET <lucas.rouretperso@gmail.com>
2022-12-02 16:38:30 +0100

    ajout du détail

--- app/src/main/java/fr/imt/henrypotier/bookDetail/BookDetailActivity.kt
+++ app/src/main/java/fr/imt/henrypotier/bookDetail/BookDetailActivity.kt
@@ -6,8 +6,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import fr.imt.henrypotier.R
 import fr.imt.henrypotier.bookList.BOOK_ID
+import fr.imt.henrypotier.data.Book
 
 class BookDetailActivity : AppCompatActivity() {
 
@@ -19,18 +21,43 @@ class BookDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_detail)
 
-        var currentBookId: Long? = null
+        var currentBookId: String? = null
+
+        //get the book id from the intent
+        val intent = intent
+        if (!intent.hasExtra(BOOK_ID)){
+            finish()
+        }
+
+        currentBookId = intent.getStringExtra(BOOK_ID)
+
+        //get the book from the view model
+        val book : Book? = currentBookId?.let { bookDetailViewModel.getBookForId(it) }
+
 
         /* Connect variables to UI elements. */
-        val bookName: TextView = findViewById(R.id.book_detail_name)
+        val bookName: TextView = findViewById(R.id.book_detail_title)
         val bookImage: ImageView = findViewById(R.id.book_detail_image)
         val bookDescription: TextView = findViewById(R.id.book_detail_description)
-        val removeBookButton: Button = findViewById(R.id.remove_button)
+        val bookPrice: TextView = findViewById(R.id.book_detail_price)
+        val addToCartButton: Button = findViewById(R.id.book_detail_add_to_cart)
 
         val bundle: Bundle? = intent.extras
         if (bundle != null) {
-            currentBookId = bundle.getLong(BOOK_ID)
+            currentBookId = bundle.getString(BOOK_ID)
         }
+
+        //set the book data to the UI elements
+        bookName.text = book?.title
+        bookDescription.text = book?.synopsis?.joinToString(separator = "") { it }
+        bookPrice.text = book?.price.toString()
+        Glide.with(this)
+            .load(book?.cover)
+            .into(bookImage)
+
+
+
+
 
         /* If currentBookId is not null, get corresponding book and set name, image and
         description */
--- app/src/main/java/fr/imt/henrypotier/data/Book.kt
+++ app/src/main/java/fr/imt/henrypotier/data/Book.kt
@@ -3,16 +3,10 @@ package fr.imt.henrypotier.data
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 
-class Book() : RealmObject {
-    @PrimaryKey
+class Book() {
     var isbn: String = ""
     var title: String = ""
-    var price: String = ""
+    var price: Int = 0
     var cover: String = ""
-    constructor(isbn: String, title: String, price: String, cover: String) : this() {
-        this.isbn = isbn
-        this.title = title
-        this.price = price
-        this.cover = cover
-    }
+    var synopsis: List<String> = listOf()
 }
